4d824d8125a648ab59a71620bf809d583402f0dc
Wilfredo Escobar <wilfredosorto75@gmail.com>
2023-03-14 10:30:24 -0600

    Add data attribute to the product edit button

--- controllers/allproducts-controller.js
+++ controllers/allproducts-controller.js
@@ -28,6 +28,7 @@ const newProduct = (name, price, imageURL, id) => {
   productPrice.innerHTML = price;
   productId.innerHTML = "#" + id;
 
+  editBtn.setAttribute("data-edit", "");
   trashBtn.setAttribute("data-trash", "");
 
   card.appendChild(editBtn);
