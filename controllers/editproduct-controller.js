f29044c91f2f6867aaa88fab5bb954bce150ca2b
Wilfredo Escobar <wilfredosorto75@gmail.com>
2023-03-23 22:05:03 -0600

    Verify that a product is selected for editing
    Through the id of the product obtained from the url

--- controllers/editproduct-controller.js
+++ controllers/editproduct-controller.js
@@ -1,2 +1,7 @@
 import { productServices } from "../services/product-services.js";
 import { validate } from "../assets/js/validations.js";
+
+// Checking if the id is undefined, if it is, it will redirect the user to the products.html page.
+if (id == undefined) {
+  window.location.replace("./products.html");
+}
